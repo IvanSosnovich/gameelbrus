57b609476289efd96c8a8a67a765740de1d454de
Ayuna <aunmiku@gmail.com>
2021-02-26 13:07:36 +0300

    add /api/elbrusgame

--- Server/app.js
+++ Server/app.js
@@ -3,6 +3,11 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const cors = require("cors");
+const User = require('./db/models/User');
+const Category = require('./db/models/Category');
+const Question = require('./db/models/Question');
+const Result = require('./db/models/Result');
+
 
 // const session = require('express-session');
 // const FileStore = require('session-file-store')(session);
@@ -34,10 +39,33 @@ app.use(cors());
 // });
 
 
+// отправляет всю игру
+app.get("/api/elbrusgame", async (req, res) => {
+  // let elbrusgame;
+  let categories;
+  let questions;
+  try {
+    categories = await Category.find();
+    questions = await Question.find().sort({'category': 1, 'price': 1}).populate('category').exec();
 
-app.get("/api/category", (req, res) => {
-  res.json(todos);
+  } catch (error) {
+    console.log('Не удалось загрузить категории.')
+  }
+
+  res.json({ categories, questions });
+
 });
+
+
+app.post("/api/register", (req,res) => {
+  const { name, email, password } = req.body;
+
+})
+
+app.get("/api/login", (req,res) => {
+
+})
+
 
 app.post("/api/todos", (req, res) => {
   const { title, priority } = req.body;
@@ -46,8 +74,6 @@ app.post("/api/todos", (req, res) => {
   todos.push(newTodo);
   res.json(newTodo);
 });
-
-
 
 
 app.listen(process.env.PORT, () => {
--- Server/db/seed.js
+++ Server/db/seed.js
@@ -1,117 +1,50 @@
-const { connect, disconnect, Cards, User } = require('./models');
+require('dotenv').config();
 
-async function main() {
-  await connect('mongodb://localhost:27017/',
-    { useNewUrlParser: true, useUnifiedTopology: true },
-    () => {
-      console.log('Успешно подключились к базе данных.');
-    });
+const mongoose = require('mongoose');
 
-  const users = [];
-  const cards = [];
-
-  //users.push(new User({ name: 'Ivan', score: 3 }));
+const Category = require('./models/Category');
+const User = require('./models/User');
+const Question = require('./models/Question');
+const Result = require('./models/Result');
 
 
-  cards.push(new Cards({
-    name: 'beatles',
-    question: 'Какого музыканта не хватало группе на ранних стадиях развития? (Баяниста, Арфиста, Барабанщика)',
-    answer: 'Барабанщика',
-  }));
+mongoose.connect(`mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@${process.env.DB_CONNECTION_ADRESS}/elbrusgame?${process.env.DB_CONNECTION_OPTIONS}`);
 
-  cards.push(new Cards({
-    name: 'beatles',
-    question: 'О каком транспорте спели «битлы»? (Оранжевом пароходе, Желтой подлодке, Красном самолёте)',
-    answer: 'Желтой подлодке',
-  }));
-
-  cards.push(new Cards({
-    name: 'beatles',
-    question: 'Как по-другому называют группу «Битлз»? (Ливерпульская четверка, Жучары, Нет другого названия)',
-    answer: 'Ливерпульская четверка',
-  }));
-
-  cards.push(new Cards({
-    name: 'friends',
-    question: 'Какая была надпись на рюкзаке Роса в детстве? (Геология - рулит!, Динозавры - рулят!, Книги - рулят!)',
-    answer: 'Геология - рулит!',
-  }));
-
-  cards.push(new Cards({
-    name: 'friends',
-    question: 'Сколько детей родила Фиби? (трех, одного, двух)',
-    answer: 'Троих',
-  }));
-
-  cards.push(new Cards({
-    name: 'friends',
-    question: 'Какие животные жили у Джо и Чендлера? (Крыса и хорек, Енот и дерево, Утка и курица)',
-    answer: 'Утка и курица',
-  }));
-
-  cards.push(new Cards({
-    name: 'words',
-    question: 'Из какого языка пришло слово “тротуар”? (Итальянский, Испанский, Французский)',
-    answer: 'Французский',
-  }));
-
-  cards.push(new Cards({
-    name: 'words',
-    question: 'Из какого языка пришло слово “гусар”? (Французский, Венгерский, Болгарский)',
-    answer: 'Венгерский',
-  }));
-
-  cards.push(new Cards({
-    name: 'words',
-    question: 'Из какого языка пришло слово “чай”? (Корейский, Китайский, Хинди)',
-    answer: 'Китайский',
-  }));
-
-  await User.insertMany(users);
-  await Cards.insertMany(cards);
-  await disconnect();
-}
-
-main();
-
-
-const mongoose = require("mongoose");
-const TypeFoto = require('../db/models/typeFoto')
-mongoose.connect('mongodb://localhost:27017/fotosite', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const typeFoto = [
+const category = [
   {
     typename: "Love story",
     description: "Услуги фотосессии по Вашим желаниям",
     price: 1000,
-    images:[{_id:'60250145412e3151a4af52a8'},{_id:'60250145412e3151a4af52a9'},{_id:'60250145412e3151a4af52aa'},{_id:'60250145412e3151a4af52c1'},{_id:'60250145412e3151a4af52c2'},{_id:'60250145412e3151a4af52c3'}]
+    images: [{ _id: '60250145412e3151a4af52a8' }, { _id: '60250145412e3151a4af52a9' }, { _id: '60250145412e3151a4af52aa' }, { _id: '60250145412e3151a4af52c1' }, { _id: '60250145412e3151a4af52c2' }, { _id: '60250145412e3151a4af52c3' }]
   },
   {
     typename: "Свадебная фотосъемка",
     description: "Услуги фотосессии по Вашим желаниям с выездом",
     price: 1000,
-    images:[{_id:'60250145412e3151a4af52ab'},{_id:'60250145412e3151a4af52ac'},{_id:'60250145412e3151a4af52ad'},{_id:'60250145412e3151a4af52be'},{_id:'60250145412e3151a4af52bf'},{_id:'60250145412e3151a4af52c0'}]
+    images: [{ _id: '60250145412e3151a4af52ab' }, { _id: '60250145412e3151a4af52ac' }, { _id: '60250145412e3151a4af52ad' }, { _id: '60250145412e3151a4af52be' }, { _id: '60250145412e3151a4af52bf' }, { _id: '60250145412e3151a4af52c0' }]
   },
   {
     typename: "Мужские портреты",
     description: "Услуги фотосессии по Вашим желаниям",
     price: 500,
-    images:[{_id:'60250145412e3151a4af52ae'},{_id:'60250145412e3151a4af52af'},{_id:'60250145412e3151a4af52b0'},{_id:'60250145412e3151a4af52bc'},{_id:'60250145412e3151a4af52bd'}]
+    images: [{ _id: '60250145412e3151a4af52ae' }, { _id: '60250145412e3151a4af52af' }, { _id: '60250145412e3151a4af52b0' }, { _id: '60250145412e3151a4af52bc' }, { _id: '60250145412e3151a4af52bd' }]
   },
   {
     typename: "Женские портреты",
     description: "Услуги фотосессии по Вашим желаниям",
     price: 500,
-    images:[{_id:'60250145412e3151a4af52b1'},{_id:'60250145412e3151a4af52b2'},{_id:'60250145412e3151a4af52b3'},{_id:'60250145412e3151a4af52ba'},{_id:'60250145412e3151a4af52bb'}]
+    images: [{ _id: '60250145412e3151a4af52b1' }, { _id: '60250145412e3151a4af52b2' }, { _id: '60250145412e3151a4af52b3' }, { _id: '60250145412e3151a4af52ba' }, { _id: '60250145412e3151a4af52bb' }]
   },
   {
     typename: "Журналы и публикации",
     description: "Представлены фотографии , опубликованные в периодических печатных изданиях и на сайтах СМИ",
     price: 500,
-    images:[{_id:'60250145412e3151a4af52b4'},{_id:'60250145412e3151a4af52b5'},{_id:'60250145412e3151a4af52b6'}, {_id:'60250145412e3151a4af52b7'},{_id:'60250145412e3151a4af52b8'},{_id:'60250145412e3151a4af52b9'}]
+    images: [{ _id: '60250145412e3151a4af52b4' }, { _id: '60250145412e3151a4af52b5' }, { _id: '60250145412e3151a4af52b6' }, { _id: '60250145412e3151a4af52b7' }, { _id: '60250145412e3151a4af52b8' }, { _id: '60250145412e3151a4af52b9' }]
   },
 
 ]
-TypeFoto.insertMany(typeFoto).then(() => {
-    mongoose.connection.close();
-  });
+
+
+Category.insertMany(category).then(() => {
+  mongoose.connection.close();
+});
