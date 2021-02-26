120a7e1c068989c66d259982e0bcee71981e5f11
Gamz <jeydey@mail.ru>
2021-02-26 13:17:34 +0300

    main page create

--- client/src/Components/App/App.jsx
+++ client/src/Components/App/App.jsx
@@ -1,19 +1,19 @@
-import './App.css';
-import React from 'react';
-import LogIn from './LogIn/Login';
-import Register from './Register/Register';
-import Questions from './Questions/Questions';
-import MainPage from './MainPage/MainPage';
-import TableResult from './TableResult/TableResult';
+import "./App.css";
+import React from "react";
+// import LogIn from './LogIn/Login';
+// import Register from './Register/Register';
+// import Questions from './Questions/Questions';
+import MainPage from "../MainPage/MainPage";
+// import TableResult from './TableResult/TableResult';
 
 function App() {
   return (
     <div div className="App">
-      <LogIn />
+      {/* <LogIn />
       <Register />
-      <Questions />
+      <Questions /> */}
       <MainPage />
-      <TableResult />
+      {/* <TableResult /> */}
     </div>
   );
 }
