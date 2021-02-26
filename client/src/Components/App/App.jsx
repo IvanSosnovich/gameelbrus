1bab60115a20f4706cf87919479897ad148358c3
Mikhail.CallInNickOff <mihalka999@gmail.com>
2021-02-26 13:08:39 +0300

    Component LOGIN

--- client/src/Components/App/App.jsx
+++ client/src/Components/App/App.jsx
@@ -1,10 +1,10 @@
 import './App.css';
 import React from 'react';
-import LogIn from './LogIn/Login';
-import Register from './Register/Register';
-import Questions from './Questions/Questions';
-import MainPage from './MainPage/MainPage';
-import TableResult from './TableResult/TableResult';
+import LogIn from '../LogIn/Login';
+import Register from '../Register/Register';
+import Questions from '../Questions/Questions';
+import MainPage from '../MainPage/MainPage';
+import TableResult from '../TableResult/TableResult';
 
 function App() {
   return (
